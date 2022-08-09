5354e973f2b3e3d90399f05656b38075d85924a4
robbotyk <09014198535a@gmail.com>
2022-08-09 16:38:57 +0900

    [add]記事の作成

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,7 @@ use Illuminate\Support\Facades\Route;
 });
 */
 Route::get('/','PostController@index');
+Route::get('/posts/create','PostController@create');
 Route::get('/posts/{post}','PostController@show');
+
+Route::post('/posts', 'PostController@store');
